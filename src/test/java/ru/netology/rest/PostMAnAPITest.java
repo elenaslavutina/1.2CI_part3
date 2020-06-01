f489d5962769afe8cc0e973a3e777494cae0348b
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-01 17:00:26 +0300

    some changes

---
+++ src/test/java/ru/netology/rest/PostMAnAPITest.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostMAnAPITest {
+    @Test
+    public void shouldPost(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body( "Herman Melville" )
+                
+        // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .log().all()
+                .statusCode(200)
+                //.body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
