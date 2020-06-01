de36c98ba1a8faf8ef074b05e9707f65b7440a66
elenaslavutina <elena.slavutina2013@gmail.com>
2020-06-01 13:11:55 +0300

    Initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+      // Given - When - Then
+      // Предусловия
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+      // Выполняемые действия
+      .when()
+          .get("/demo/accounts")
+      // Проверки
+      .then()
+          .statusCode(200)
+          .body("[2].name", equalTo("Текущий зарплатный счёт"))
+        ;
+
+    }
+}
